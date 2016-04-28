190346967ae394a28faa66e4b4ec9a8a91e97d88
s1240060_yamada <s1240060@u-aizu.ac.jp>
2016-04-28 19:37:00 +0900

    solve all problem

---
+++ if.rb
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+if true then
+  puts "true is true"
+end
+
+if false then
+  puts "false is false"
+end
+
+if 1< 2 then
+  puts "aaaaaa"
+end
+
+if 1== 2 then
+  puts "bbbbbbb"
+end
+
+if 1== 2 then 
+  puts "ccccccc"
+else 
+  puts "ddddddd"
+end
+
+x= 12345
+if x> 100 then 
+  puts "x is 100 over"
+elsif x% 2== 0 then 
+  puts "x is even" 
+else 
+  puts "x is odd" 
+end 
+
+
