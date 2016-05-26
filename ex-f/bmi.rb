ba2fd87c5e1a42e1b48526c4ddd773e1b5bd17c4
s1240060_yamada <s1240060@u-aizu.ac.jp>
2016-05-26 19:13:48 +0900

    learn function ver.2

--- ex-f/bmi.rb
+++ ex-f/bmi.rb
@@ -1,9 +1,14 @@
+t,w= gets.split(" ")
+t=t.to_i
+w=w.to_i
 def cal_bmi(tall, weight)
     weight/ ((tall.to_f/100) * (tall.to_f/100))
 end
 def cal_weight(tall)
     (tall.to_f/100)*(tall.to_f/100)*22
 end
-puts cal_bmi(170,60)
-puts cal_weight(170)
+# puts cal_bmi(170,60)
+# puts cal_weight(170)
 
+puts cal_bmi(t,w)
+puts cal_weight(t)
